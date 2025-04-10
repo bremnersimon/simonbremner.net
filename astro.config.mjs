8ceae06f98cf5858b0b08199afa73172a17fcdae
Simon Bremner <simon.bremner@secureideas.com>
2025-04-10 09:37:02 -0400

    Cleaned up SEO tags and fixed the toggle button

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,25 +6,23 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
-	site: "https://www.simonbremner.net",
-	integrations: [
-		react(),
-		sanity({
-			projectId: "hzdtear2",
-			dataset: "production",
-			apiVersion: "2024-01-01",
-			useCdn: false,
-		}),
-		sitemap(),
-	],
-	vite: {
-		// @ts-ignore
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"@": path.resolve("./src"),
-			},
-		},
-	},
+    site: "https://www.simonbremner.net",
+    integrations: [react(), sanity({
+        projectId: "hzdtear2",
+        dataset: "production",
+        apiVersion: "2024-01-01",
+        useCdn: false,
+		}), sitemap(), partytown()],
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve("./src"),
+            },
+        },
+    },
 });
