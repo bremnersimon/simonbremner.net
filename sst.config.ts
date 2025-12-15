c5f6bf736dd401e1e2f8a54cb8e32157acadaf63
bremnersimon <bremner94@gmail.com>
2025-12-15 09:16:08 -0500

    Deployed SST successfully

--- sst.config.ts
+++ sst.config.ts
@@ -1,22 +1,25 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "SimonBremnerNet",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-      providers: {
-        aws: {
-          region: "us-east-1",
-          profile: "Simonsberg"
-          
-        }
-      }
-    };
-  },
-  async run() {
-    new sst.aws.Astro("simonbremner-net");
-  },
+	app(input) {
+		return {
+			name: "SimonBremnerNet",
+			removal: input?.stage === "production" ? "retain" : "remove",
+			protect: ["production"].includes(input?.stage),
+			home: "aws",
+			providers: {
+				aws: {
+					profile: "Simonsberg",
+				},
+			},
+		};
+	},
+	async run() {
+		new sst.aws.Astro("simonbremner-net", {
+			domain: {
+				name: "simonbremner.net",
+				redirects: ["www.simonbremner.net"],
+			},
+		});
+	},
 });
