4aea603f0a4389ae7d07db0a3c6a4241097219ab
Simon Bremner <bremner94@gmail.com>
2026-08-04 10:58:00 -0400

    Added staging deployment method

--- sst.config.ts
+++ sst.config.ts
@@ -4,8 +4,15 @@ export default $config({
 	app(input) {
 		return {
 			name: "SimonBremnerNet",
-			removal: input?.stage === "production" ? "retain" : "remove",
-			protect: ["production"].includes(input?.stage),
+			removal:
+				input?.stage === "production"
+					? "retain"
+					: input?.stage === "staging"
+						? "retain"
+						: "remove",
+			protect:
+				["production"].includes(input?.stage) ||
+				["staging"].includes(input?.stage),
 			home: "aws",
 			providers: {
 				aws: {
@@ -17,8 +24,19 @@ export default $config({
 	async run() {
 		new sst.aws.Astro("simonbremner-net", {
 			domain: {
-				name: "simonbremner.net",
-				redirects: ["www.simonbremner.net"],
+				name:
+					$app.stage === "production"
+						? "simonbremner.net"
+						: $app.stage === "staging"
+							? "staging.simonbremner.net"
+							: "dev.simonbremner.net",
+				redirects: [
+					$app.stage === "production"
+						? "www.simonbremner.net"
+						: $app.stage === "staging"
+							? "www.staging.simonbremner.net"
+							: "www.dev.simonbremner.net",
+				],
 			},
 		});
 	},
